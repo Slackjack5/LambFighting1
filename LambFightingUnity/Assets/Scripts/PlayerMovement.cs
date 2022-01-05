02d452f8a7a00bf26b8520fb50f0c8c3838d0702
Lambda <Lambeleven@gmail.com>
2022-01-05 04:31:41 -0800

    Mirrored movement on opposite side

--- LambFightingUnity/Assets/Scripts/PlayerMovement.cs
+++ LambFightingUnity/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 
 {
+    [SerializeField] private GameObject Enemy;
+    private int playerSide;
     private Rigidbody2D body;
     private Animator anim;
     [SerializeField] private float runSpeed;
@@ -18,20 +20,28 @@ public class PlayerMovement : MonoBehaviour
     }
 
     private void Update()
+
     {
+
+        if (gameObject.transform.position.x < Enemy.transform.position.x) { playerSide = -1; } else { playerSide = 1; }
+
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         body.velocity = new Vector2(horizontalInput * runSpeed, body.velocity.y);
 
-        //Flip player when moving opposite direction
-        if (horizontalInput > 0.01f)
-            transform.localScale = new Vector3(-1, 1, 1);
-        else if (horizontalInput < -0.01f)
-            transform.localScale = Vector3.one;
-
+        if (playerSide == -1)
+        {
+            anim.SetBool("Walk", horizontalInput > 0.01f);
+            anim.SetBool("BackWalk", horizontalInput < -0.01f);
+            gameObject.transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (playerSide == 1)
+        {
+            anim.SetBool("Walk", horizontalInput < -0.01f);
+            anim.SetBool("BackWalk", horizontalInput > 0.01f);
+            gameObject.transform.localScale = Vector3.one;
+        }
         if (Input.GetKey(KeyCode.W))
             body.velocity = new Vector2(body.velocity.x, runSpeed);
 
-        //Set Animator parameters
-        anim.SetBool("Walk", horizontalInput != 0);
     }
 }
